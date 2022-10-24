4c165c0c89d63a88bc3ed2527b1e6fa73eb8d062
Seamoon Pandey <hackedasocial@gmail.com>
2022-10-24 23:54:37 +0545

    solution_for_17inC

---
+++ problem_17.c
@@ -0,0 +1,25 @@
+/*If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.*/
+
+#include <stdio.h>
+#include <string.h>
+#include "num2word.h"
+
+int main()
+{
+    char word[100];
+    int sum=0;
+    for (int i = 1; i <= 1000; i++)
+    {
+        strcpy(word, numberWords(i));
+        sum+=strlen(word);
+    }
+    printf("%d",sum);
+        // strcpy(word, numberWords(100000));
+        // printf("%s\n", word);
+    return 0;
+}
